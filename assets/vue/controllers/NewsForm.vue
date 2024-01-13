fd0f0944076bd85b14abc22416b7a0f6c5ba4036
Kévin R <rifakev@gmail.com>
2024-01-13 22:11:46 +0100

    KR - Configuration de la newsletter

--- assets/vue/controllers/NewsForm.vue
+++ assets/vue/controllers/NewsForm.vue
@@ -1,12 +1,48 @@
 <template>
-    <form class="position-relative">
-        <input class="news-input" type="email" name="news" id="news" placeholder="Inscrivez vous à notre newsletter" aria-label="Inscrivez vous à notre newsletter">
-        <button class="btn-news-submit position-absolute" type="submit">&#8594</button>
+    <form id="news-form" class="position-relative">
+        <input class="news-input" type="email" name="news" id="news" v-model="email" placeholder="Inscrivez vous à notre newsletter" aria-label="Inscrivez vous à notre newsletter">
+        <button class="btn-news-submit position-absolute" type="submit" @click="createNewsContact">&#8594</button>
     </form>
 </template>
 
 <script>
 export default {
-    
+    el: "#news-form",
+    data() {
+        return {
+            formData: {
+                email: this.email,
+            }
+        };
+    },
+    methods: {
+        createNewsContact(event) {
+            event.preventDefault();
+
+            if (this.email) {
+                const requestOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.formData)
+                };
+
+                fetch('https://api.brevo.com/v3/contacts', requestOptions)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`Erreur HTTP : ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('Contact ajouté à la liste BREVO :', data);
+                    })
+                    .catch(error => {
+                        console.error('Erreur BREVO :', error.message);
+                    });
+            }
+        }
+    },
 }
 </script>
